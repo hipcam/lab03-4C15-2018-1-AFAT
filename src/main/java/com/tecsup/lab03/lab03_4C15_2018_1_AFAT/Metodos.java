4b4ea0abbc9222dc5185b0ef714b37dbf4bee27a
Alumno <Alumno@LAB701-13>
2018-02-26 11:39:42 -0500

    colaborador-Guerra

--- src/main/java/com/tecsup/lab03/lab03_4C15_2018_1_AFAT/Metodos.java
+++ src/main/java/com/tecsup/lab03/lab03_4C15_2018_1_AFAT/Metodos.java
@@ -20,5 +20,23 @@ public class Metodos {
 	        JOptionPane.showMessageDialog(null, "La resta es: "+c);
 		
 	}
+	
+	public static void multiplicacion(){
+		
+	      int  a=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer valor para la resta"));
+	      int  b=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo valor para la resta"));
+	      int  c=a*b;
+	        JOptionPane.showMessageDialog(null, "El producto es : "+c);
+		
+	}
+	public static void division(){
+		
+	      int  a=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer valor para la resta"));
+	      int  b=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo valor para la resta"));
+	      int  c=a/b;
+	        JOptionPane.showMessageDialog(null, "La division es : "+c);
+		
+	}
+	
 }
 
