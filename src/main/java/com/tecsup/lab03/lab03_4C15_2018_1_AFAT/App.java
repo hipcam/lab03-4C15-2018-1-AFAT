638ba9513fcafc1222b099bc8057bc5dffe9341e
Fernanda <alessandrafernanda.alvarado@tecsup.edu.pe>
2018-02-26 11:07:00 -0500

    Segundo cambio

---
+++ src/main/java/com/tecsup/lab03/lab03_4C15_2018_1_AFAT/App.java
@@ -0,0 +1,18 @@
+package com.tecsup.lab03.lab03_4C15_2018_1_AFAT;
+
+
+import com.tecsup.lab03.lab03_4C15_2018_1_AFAT.Metodos;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      Metodos met=new Metodos();
+     met.Suma();
+     met.Resta();
+    }
+   
+}
---
+++ src/main/java/com/tecsup/lab03/lab03_4C15_2018_1_AFAT/Metodos.java
@@ -0,0 +1,24 @@
+package com.tecsup.lab03.lab03_4C15_2018_1_AFAT;
+
+import javax.swing.JOptionPane;
+
+public class Metodos {
+ 
+	public void Suma(){
+       
+      int  a=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer valor para la suma"));
+      int  b=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo valor para la suma"));
+      int  c=a+b;
+        JOptionPane.showMessageDialog(null, "La suma es: "+c);
+		
+	}
+	public void Resta(){
+		
+	      int  a=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer valor para la resta"));
+	      int  b=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo valor para la resta"));
+	      int  c=a-b;
+	        JOptionPane.showMessageDialog(null, "La resta es: "+c);
+		
+	}
+}
+
